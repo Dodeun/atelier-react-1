38283502d43b5a46717a462dd08184e6a786b897
Burdon Jordan <burdon.jordan@gmail.com>
2025-03-27 16:10:42 +0100

    Update

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import AvatarStyles from "./../styles/Avatar.module.css";
 
 interface avatarProps {
@@ -10,6 +11,12 @@ interface avatarProps {
 
 function Avatar({ simpson }: avatarProps) {
 	const { image, firstName, lastName } = simpson;
+	const [donutCount, setDonutCount] = useState<number>(0);
+
+	const handleClick = () => {
+		setDonutCount((dc) => dc + 1);
+	};
+
 	return (
 		<>
 			<figure className={AvatarStyles.card}>
@@ -21,6 +28,16 @@ function Avatar({ simpson }: avatarProps) {
 				<figcaption className={AvatarStyles.cardTitle}>
 					{firstName}{" "}
 					<span className={AvatarStyles.cardSTitleSecondary}>{lastName}</span>
+					<button
+						className={AvatarStyles.cardBtn}
+						type="button"
+						onClick={() => {
+							handleClick();
+							handleClick();
+						}}
+					>
+						🍩 {donutCount}
+					</button>
 				</figcaption>
 			</figure>
 		</>
